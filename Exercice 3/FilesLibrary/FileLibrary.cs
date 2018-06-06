6c217cb5ae2370c6350050ad1a5faa1b2a469143
Anis <anis.youssef@soat.fr>
2018-06-06 14:06:11 +0200

    Exercice 3 - Version 2

--- Exercice 3/FilesLibrary/FileLibrary.cs
+++ Exercice 3/FilesLibrary/FileLibrary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Xml;
 
 namespace FilesLibrary
 {
@@ -12,6 +13,24 @@ namespace FilesLibrary
                 if (File.Exists(path))
                 {
                     return File.ReadAllText(path);
+                }
+                throw new FileNotFoundException();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static XmlDocument ReadXmlFile(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(path);
+                    return doc;
                 }
                 throw new FileNotFoundException();
             }
