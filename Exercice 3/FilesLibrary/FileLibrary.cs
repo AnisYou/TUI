fa80faba0791e7cd302c5a3ae5b7a18b7cb63bff
Anis <anis.youssef@soat.fr>
2018-06-06 12:02:27 +0200

    Exercice 3 - Version 1

---
+++ Exercice 3/FilesLibrary/FileLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace FilesLibrary
+{
+    public class FileLibrary
+    {
+        public static string ReadTextFile(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    return File.ReadAllText(path);
+                }
+                throw new FileNotFoundException();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
